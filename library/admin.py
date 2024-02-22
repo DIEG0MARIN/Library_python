267226a50a63e7ccb57f70422cf07714b25ebe45
DIEG0MARIN <diegof.maring@konradlorenz.edu.co>
2024-02-22 18:46:52 -0500

    Correción y modificación de compatibilidad con imagenes

--- library/admin.py
+++ library/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import Libro
 # Register your models here.
 
+# Este espacio es para que el administrador manipule información
 admin.site.register(Libro)
 #admin.site.register(Prestamos)
--- library/views.py
+++ library/views.py
@@ -1,25 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Libro
 from .forms import LibroForm
 # Create your views here.
 
+##ACCESO A LAS PAGINAS DEL SITIO 
 def inicio(request):
-    return render(request, 'inicio.html')
+    return render(request, 'paginas/inicio.html')
 
 def nosotros(request):
-    return render(request, 'nosotros.html')
+    return render(request, 'paginas/nosotros.html')
+
+
+# ACCESO A LOS LIBROS
 
 def libros(request):
     libros = Libro.objects.all()
     return render(request,'libros/index.html', {'libros': libros})
 
-def crear_libro(request):
-    formulario = LibroForm(request.POST or None)
-    return render(request, 'libros/crear.html')
+def crear(request):
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros')
+    return render(request, 'libros/crear.html',{'formulario' : formulario})
 
-def editar(request):
-    return render(request, 'libros/editar.html')
+def editar(request, id):
+    libro = Libro.objects.get(id=id)
+    formulario = LibroForm(request.POST or None, request.FILES or None, instance= libro ) 
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('libros')
+    return render(request, 'libros/editar.html',{'formulario' : formulario } )
 
-#CONTINUAR EN MINUTO 1:50:00 RELIZAR EN GITHUB RESTO DE CODIFICACION
-#Y LUEGO TRAER DETALLES CON UN PULL
+def eliminar(request, id):
+    libro = Libro.objects.get(id=id)
+    libro.delete()
+    return redirect('libros')
+
+
